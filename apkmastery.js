fc79c184dc90d5deb48efec0f55e8ae6b92a136e
Sreerag-G <145847712+Sreerag-G@users.noreply.github.com>
2023-09-24 19:01:03 +0530

    ok
    proceed

---
+++ apkmastery.js
@@ -0,0 +1,96 @@
+<div class="elementor-widget-wrap">
+<div class="elementor-element elementor-element-16f2a90e elementor-widget elementor-widget-gamez-section_title" data-id="16f2a90e" data-element_type="widget" data-widget_type="gamez-section_title.default">
+<div class="elementor-widget-container">
+<div class="gamez-section-title">
+<h2 class="title-wrapper">
+<i class="gamez-section-title-icon fa fa-bullseye"></i>
+Latest News</h2>
+</div> </div>
+</div>
+<div class="elementor-element elementor-element-1e250a46 elementor-widget elementor-widget-gamez-recent_posts" data-id="1e250a46" data-element_type="widget" data-widget_type="gamez-recent_posts.default">
+<div class="elementor-widget-container">
+<div class="gamez-recent-with-thumb">
+<div class="row">
+<div class="layout-2">
+<div class="col-md-6">
+<div class="media">
+<div class="media-top">
+<figure>
+<a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/08/assassins-creed-origins-ptolemy-xiii/">
+<img width="640" height="338" src="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2019/05/img-3-1024x541.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2019/05/img-3-1024x541.jpg 1024w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2019/05/img-3-300x159.jpg 300w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2019/05/img-3-768x406.jpg 768w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2019/05/img-3-600x317.jpg 600w" sizes="(max-width: 640px) 100vw, 640px"> </a>
+</figure>
+</div>
+<div class="media-body">
+<div class="content-desc">
+<div class="content-meta">
+<span><i class="fa fa-calendar"></i> June 08, 2010</span>
+<span class="comments-link" title="0 Comment"><i class="fa fa-comment"></i> <a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/08/assassins-creed-origins-ptolemy-xiii/#respond">0 Comment</a></span> </div>
+<h4 class="media-heading"><a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/08/assassins-creed-origins-ptolemy-xiii/">Assassin’s Creed: Origins –…</a> </h4>
+<div class="media-content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy…</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+<div class="media">
+<div class="media-top">
+<figure>
+<a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/02/crackdown-3-release-date-trailers-and-preview/">
+<img width="640" height="360" src="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/06/img-7-1024x576.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/06/img-7-1024x576.jpg 1024w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/06/img-7-300x169.jpg 300w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/06/img-7-768x432.jpg 768w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/06/img-7-600x338.jpg 600w" sizes="(max-width: 640px) 100vw, 640px"> </a>
+</figure>
+</div>
+<div class="media-body">
+<div class="content-desc">
+<div class="content-meta">
+<span><i class="fa fa-calendar"></i> June 02, 2010</span>
+<span class="comments-link" title="0 Comment"><i class="fa fa-comment"></i> <a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/02/crackdown-3-release-date-trailers-and-preview/#respond">0 Comment</a></span> </div>
+<h4 class="media-heading"><a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/06/02/crackdown-3-release-date-trailers-and-preview/">Crackdown 3 release date,…</a> </h4>
+<div class="media-content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy…</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+<div class="media">
+<div class="media-top">
+<figure>
+<a href="https://demo.themexpert.com/wordpress/gamez/game-review/2012/05/07/far-cry-5-launches-on-xbox-one/">
+<img width="640" height="360" src="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2012/05/farcry-1024x576.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2012/05/farcry.jpg 1024w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2012/05/farcry-300x169.jpg 300w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2012/05/farcry-768x432.jpg 768w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2012/05/farcry-600x338.jpg 600w" sizes="(max-width: 640px) 100vw, 640px"> </a>
+</figure>
+</div>
+<div class="media-body">
+<div class="content-desc">
+<div class="content-meta">
+<span><i class="fa fa-calendar"></i> May 07, 2012</span>
+<span class="comments-link" title="0 Comment"><i class="fa fa-comment"></i> <a href="https://demo.themexpert.com/wordpress/gamez/game-review/2012/05/07/far-cry-5-launches-on-xbox-one/#respond">0 Comment</a></span> </div>
+<h4 class="media-heading"><a href="https://demo.themexpert.com/wordpress/gamez/game-review/2012/05/07/far-cry-5-launches-on-xbox-one/">Far Cry 5 launches…</a> </h4>
+<div class="media-content"> Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Sed cras ornare arcu dui vivamus. Laoreet id donec ultrices…</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+<div class="media">
+<div class="media-top">
+<figure>
+<a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/07/02/post-format-audio/">
+<img width="640" height="360" src="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/07/img-5-1024x576.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/07/img-5-1024x576.jpg 1024w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/07/img-5-300x169.jpg 300w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/07/img-5-768x432.jpg 768w, https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2010/07/img-5-600x338.jpg 600w" sizes="(max-width: 640px) 100vw, 640px"> </a>
+</figure>
+</div>
+<div class="media-body">
+<div class="content-desc">
+<div class="content-meta">
+<span><i class="fa fa-calendar"></i> July 02, 2010</span>
+<span class="comments-link" title="0 Comment"><i class="fa fa-comment"></i> <a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/07/02/post-format-audio/#respond">0 Comment</a></span> </div>
+<h4 class="media-heading"><a href="https://demo.themexpert.com/wordpress/gamez/game-review/2010/07/02/post-format-audio/">Frostpunk Review: Damned If…</a> </h4>
+<div class="media-content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy…</div>
+</div>
+</div>
+</div>
+</div>
+</div> 
+</div> 
+</div>
+</div>
+</div>
+</div>
